2baedc06854742591a13c90d3631a3d4ce00624a
Sérgio Matheus <sergiomatheuss09@gmail.com>
2025-03-07 10:27:29 -0300

    Revert "testes"
    This reverts commit 98f013ac81eb8cca97c16155f3770bfcaf705fe8.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,13 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
+import { processFile } from './storage/process-file/resource';
 import { data } from './data/resource';
 
 
 defineBackend({
   auth,
   storage,
-  data
+  data,
+  processFile
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { processFile } from "../storage/process-file/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -12,6 +13,14 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.owner()]),
+      
+  processFile: a
+    .query()
+    .arguments({
+      file: a.string(),
+    })
+    .returns(a.string())
+    .handler(a.handler.function(processFile)),
 });
 
 
