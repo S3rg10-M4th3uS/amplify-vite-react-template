1c0e87024a46adc50b51c04e7466e8e3a259de46
Sérgio Matheus <sergiomatheuss09@gmail.com>
2025-03-07 10:30:21 -0300

    Revert "testes"
    This reverts commit 76070ea87a758c7deda8bbe1ae137f96aab6a5e9.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { DefaultFileUploaderExample } from './fileUploader';
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 
-
+const client = generateClient<Schema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
 
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
 
   return (
     <main>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
-        <DefaultFileUploaderExample/>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 Task completada
+      </div>
       <button onClick={signOut}>Deslogar</button>
     </main>
   );
